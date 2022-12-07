6aad6a2819678c75ace5eb250db6bf404fec2d19
Adrián Blanco Fernández <adrianblfe@gmail.com>
2022-12-07 21:56:33 +0100

    feat: init details player view

---
+++ src/DetailsPlayer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Detalles jugador</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DetailsPlayer'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
